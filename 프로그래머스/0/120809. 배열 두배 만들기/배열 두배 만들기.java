3e4273593d711e3f03748bff5050553dde3db7c6
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-06-25 17:11:25 +0900

    [level 0] Title: 배열 두배 만들기, Time: 14.90 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,15 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        //분석
-        //정수 배열 numbers, numbers의 각 원소에 두배한 원소를 가진 배열을 return
-
-        //규칙
-        //numbers 배열을 반복문을 통해 2배씩 곱해준다.
-
-        //코딩화
-        for (int i = 0; i <numbers.length ; i++) {
-            numbers[i] *=2;
-        }
-        return numbers;
+        return Arrays.stream(numbers).map(i -> i * 2).toArray();
     }
 }
