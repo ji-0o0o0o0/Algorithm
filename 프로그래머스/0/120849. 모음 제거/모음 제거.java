aaa6e2cf313d9ac99bfabc65f20695a0a741261c
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-07-11 23:19:28 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -5,14 +5,7 @@ class Solution {
             //규칙
             //a, e, i, o, u 중에 겹치는거 빼고 글자를 더해주면 될듯?
             //코딩화
-            //1. 모음 문자열을 만든다
-            String vowel = "aeiou";
-            StringBuilder answer = new StringBuilder();
-            for(char c: my_string.toCharArray()){
-                if(!vowel.contains(String.valueOf(c))){//모음이 포함하지 않으면 answer에 추가
-                    answer.append(c);
-                }
-            }
-            return answer.toString();
+            //정규화를 이용해서 모음을 빈값으로 대체한다.
+            return my_string.replaceAll("[aeiou]", "");
         }
 }
