cd2352a71c0c1707bff6dcb0246bfaa864964455
ji-0o0o0o0 <tlswldud328@gmail.com>
2025-08-14 23:53:38 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+             int last = num_list[num_list.length - 1];
+        int beforeLast = num_list[num_list.length - 2];
+
+        int[] answer = Arrays.copyOf(num_list, num_list.length + 1);
+        answer[answer.length - 1] = (last > beforeLast) ? (last - beforeLast) : (last * 2);
+
+        return answer;
+        }
+}
