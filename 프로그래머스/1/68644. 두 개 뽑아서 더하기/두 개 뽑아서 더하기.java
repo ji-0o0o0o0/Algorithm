f6d77ee76d744cfab96ddc8f133c7321f1530707
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-01-23 23:49:54 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.21 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers) {
          /*문제
         * 정수 배열 numbers 에서 다른 인뎃스에 있는 2개의 수 뽑아 더해 만들 수 있는 모든 수를 벼열에 오름차순으로 담아 반환*/
-        ArrayList<Integer> answer = new ArrayList<>();
+        HashSet<Integer> answer = new HashSet<>();
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
-                if(answer.contains(numbers[i]+numbers[j])){continue;}
                 answer.add(numbers[i]+numbers[j]);
             }
         }
