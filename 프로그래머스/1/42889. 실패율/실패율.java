0ec8167ab88bd50534a1da287a18bf1c781ff370
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-01-27 12:16:01 +0900

    [level 1] Title: 실패율, Time: 10.44 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,39 +2,27 @@ import java.util.*;
 import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int N, int[] stages) {
-        double[][] stage = new double[N][2];
-       
-       //각 스테이지 별 인원수 미리 카운팅
-        int[] cnt = new int[N+2];
-        for(int s : stages){
-            cnt[s]++;
+         //1. 스테이지별 도전자 수를 구함
+        int[] challenger =new int[N+2];
+        for (int stage : stages) {
+            challenger[stage]++;
         }
         
-        //실패율 계산
-        int total = stages.length;
-        for (int i = 0; i <N ; i++) {
-            if(total==0){
-                stage[i][0]=0;
-            }else {
-                stage[i][0]=cnt[i+1]/(double)total;
+        //2. 스테이지 별 실패한 사용자 수 계산
+        HashMap<Integer,Double> fails = new HashMap<>();
+        double total = stages.length;
+        
+        //3. 각스테이지 돌면서 실패율 계산
+        for (int i = 1; i <=N ; i++) {
+            if(challenger[i]==0){
+                fails.put(i,0.);
+            }else  {
+                fails.put(i,(double)challenger[i]/total);
+                total-=challenger[i];
             }
-            stage[i][1]=i+1;
-            total-= cnt[i+1];
         }
-        
-        //장렬
-       Arrays.sort(stage,(a,b)->{
-           if (b[0]!=a[0]){
-               return Double.compare(b[0],a[0]);//실패율 내림차순
-           }
-           return Double.compare(a[1],b[1]);//스테이지 번호 오름차순
-       });
 
-        //정렬된 순서대로 스테이지 번호만 추출
-        int[] ans = new int[N];
-        for (int i = 0; i < N; i++) {
-            ans[i]=(int)stage[i][1];
-        }
-        return ans;
+        //실패율이 높은 스테이지부터 내림차순으로 정렬
+        return fails.entrySet().stream().sorted((o1,o2)-> Double.compare(o2.getValue(),o1.getValue())).mapToInt(Map.Entry::getKey).toArray();
     }
 }
