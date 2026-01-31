4981443e6dea6c3dd68b70f0a9d0c3c3593ce82a
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-01-31 23:41:08 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.20 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,22 +1,20 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        //분석
-        //숫자와 "Z"가 공백으로 구분되어 담긴 문자열
-        //문자열에 있는 숫자를 차례대로 더하는데 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다
-        //규칙
-        //s 를 배열로 만들어서 하나씩 answer에 넣어준다. Z가 나오면 바로전꺼를 뺀다.
-        //코딩화
-        //1. 배열 만들기
-        String[] array = s.split(" ");
-        int answer = 0;
-        //2. 하나씩 더하는데 Z면 이전꺼를 빼준다.
-        for (int i = 0; i < array.length ; i++) {
-            if(array[i].equals("Z")){
-                answer-=Integer.parseInt(array[i-1]);
+       Deque<Integer> stack = new ArrayDeque<>();
+        String[] list = s.split(" ");
+
+        for(String s1 : list){
+            if(s1.equals("Z")&&!stack.isEmpty()){
+                stack.pop();
             }else{
-                answer+=Integer.parseInt(array[i]);
+                stack.push(Integer.parseInt(s1));
             }
         }
-        return answer;
+        int ans = 0;
+        while(!stack.isEmpty()){
+            ans += stack.pop();
+        }
+        return ans;
     }
 }
