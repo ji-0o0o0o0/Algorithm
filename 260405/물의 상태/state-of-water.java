afbf36a7e8f4ef86b37aaeaa563dccea15a6f4d3
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-05 00:20:44 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260405/물의 상태/state-of-water.java
+++ 260405/물의 상태/state-of-water.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println(a<0?"ice":(a>100?"vapor":"water"));       
+        System.out.println(a<0?"ice":(a>=100?"vapor":"water"));       
     }
 }
