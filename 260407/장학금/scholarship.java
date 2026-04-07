1bf5f966c43c8ca3a373bdf29b3136d19017fcc0
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-07 19:56:31 +0900

    Title: 장학금, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/장학금/scholarship.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+	    int a  = sc.nextInt();
+	    int b = sc.nextInt();
+
+        if(a<90) {
+            System.out.println(0);
+            return;
+        }
+        
+
+        System.out.println(b>=95?100000:(b>=90?50000:0));
+    }
+}
