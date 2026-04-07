1e16390ddfe03426b8e1a3043ab63c0558330d7d
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-07 19:53:06 +0900

    Title: 수의 계절은, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/수의 계절은/season-of-num.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+	    int m  = sc.nextInt();
+
+        System.out.println(m==12||m<=2?"Winter":(m<=5?"Spring":(m<=8?"Summer":"Fall")));
+
+    }
+}
