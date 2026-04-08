c2262089468ce19099303bed4c40bfc7fc4327fe
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-08 17:18:20 +0900

    Title: 윤년인가, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/윤년인가/is-leap-year.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+	    int y  = sc.nextInt();
+
+        
+        System.out.println(!(y%4!=0||(y%100==0&&y%400!=0)));
+    }
+}
