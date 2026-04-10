e37b02ab82691b80b9a25b83d802584b83b2b444
codetree-auto <codetree-auto@users.noreply.github.com>
2026-04-10 15:55:22 +0000

    auto: move codetree folders

--- 260410/감소시키며 출력하기 2/decrease-and-print-2.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
-        int i = 26;
-        while(i>=10){
-            System.out.print(i-- + " ");
-        }
-    }
-}
