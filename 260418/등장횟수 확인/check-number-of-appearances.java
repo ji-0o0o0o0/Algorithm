ed058613d965723bcabe61fcbd9c8feef9e5a594
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-18 20:55:09 +0900

    Title: 등장횟수 확인, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260418/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = 0;
+
+        for(int i =0;i<5;i++){
+            int n = sc.nextInt();
+            cnt+=n%2==0?1:0;
+        }
+
+        System.out.println(cnt);
+    }
+}
