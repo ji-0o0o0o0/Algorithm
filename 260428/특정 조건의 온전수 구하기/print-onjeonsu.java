d2708a7a3f40312955bd9f05565ac2165ae326bc
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-28 01:49:50 +0900

    Title: 특정 조건의 온전수 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260428/특정 조건의 온전수 구하기/print-onjeonsu.java
+++ 260428/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=1;i<=n;i++){
-            if(i%2==0||(i+"").charAt(0)=='5'||(i%3==0&&i%9!=0))continue;
+            if(i%2==0||i % 10 == 5||(i%3==0&&i%9!=0))continue;
             System.out.print(i+" ");
         }
 
