03376001d378ea3c30105e5047df2bb61e2dafdf
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-04-27 21:44:04 +0900

    Title: 총합 구하기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260427/총합 구하기/find-the-total.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sumVal=0;
+        for(int i=a;i<=b;i++){
+            if(i%6==0&&i%8!=0)sumVal+=ㅑ;
+        }
+        System.out.printf("%d ",sumVal);
+    }
+}
