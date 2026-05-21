3b08c4fd2ef513f05009f380bcc0402bd23b7a20
ji-0o0o0o0 <tlswldud328@gmail.com>
2026-05-22 00:31:58 +0900

    [Passed] 별표 출력하기 4 (127ms, 10MB)

---
+++ codetree/trail1/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i<2*n-1;i++){
+            if(i<n){
+                for(int j =n-i; j>0;j--){
+                    System.out.print("* ");
+                }
+            }else{
+                for(int j =2*n-i-2; j<n;j++){
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
